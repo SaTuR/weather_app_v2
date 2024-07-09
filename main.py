dff3e802eaf43bcb3848f5ff42fd88ec950f981d
SaTuR <satur_cordova@hotmail.com>
2024-07-08 21:16:25 -0500

    first commit

--- main.py
+++ main.py
@@ -45,5 +45,9 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
+@app.route("/change_password")
+def change_password():
+    pass
+
 if __name__ == "__main__":
     app.run(debug=True)
